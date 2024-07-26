2ab253a33167f6ce3d514665537a3035a0f0a331
78svak4 <Ziga.Pirc@live.com>
2024-07-26 19:24:04 +0200

    Add files via upload

--- naloga2.pas
+++ naloga2.pas
@@ -2,9 +2,9 @@ program naloga2;
 //x je kuhar 1(5/porcijo), y je kuhar 2(9/porcijo)
 var rezultat, x, y: integer;
 begin
- for y:=1 to 2000 do
+ for y:=0 to 2000 do
   begin
-   for x:=1 to 10000 do
+   for x:=0 to 10000 do
     begin
      if((y * 9 + x * 5) = 10000) then rezultat:= rezultat + 1;
     end;
